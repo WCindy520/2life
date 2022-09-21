e8b98789108afc2763d2bb2a64ba545099c84268
Willis Lin <Willis_lin@wistron.com>
2022-09-21 19:33:45 +0800

    update more photos

--- dist/scripts.js
+++ dist/scripts.js
@@ -856,7 +856,7 @@ $(document).ready(function() {
 	};
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Nov 14, 2022 12:00:00").getTime();
+		var countDownDate = new Date("Nov 17, 2022 12:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
